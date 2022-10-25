04d8ca94783f8186aab8bc929b24cc89af2c70bf
dgmolokov <dg.molokov@gmail.com>
2022-10-25 12:15:43 +0700

    add PlayLevel

--- src/ru/geekbrains/guess_the_number/Main.java
+++ src/ru/geekbrains/guess_the_number/Main.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Ваша задача угадать число.");
-        int range = 100;
+        int range = 30;
         int number = (int) (Math.random() * range);
+        playLevel(range, number);
+        scanner.close();
+    }
+
+    private static void playLevel(int range, int number) {
         while (true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
@@ -21,6 +27,5 @@ public class Main {
                 System.out.println("Загаданное число больше");
             }
         }
-        scanner.close();
     }
 }
